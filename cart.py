46066e2e0dcaed451fa4bd05565bd3eece038eee
ziyodaadbuvaliyeva <ziyodaadbuvaliyeva@gmail.com>
2025-06-01 05:35:40 +0300

    Update cart.py

--- cart.py
+++ cart.py
@@ -11,4 +11,26 @@ def show_cart(cart, products):
         for product in products:
             if product['id'] in cart:
                 print(f"{product['id']}. {product['name']} - ${product['price']} | Stock: {product['stock']}")
+                
+def remove_product(cart,products):
+    product_id=int(input('olib tashlamoqchi bo'lgan mahsulot ID sini kiriting:'))
+    if product-id in cart:
+       cart.remove(product-id)
+       print(f'ID{product_id} olib tashlandi.')
+    else:
+       print('bu mahsulot savatda yo'q.')
+def show_check(cart,products):
+    total=o
+    print("------Check menusi---------")
+    for product_id in cart:
+        for product in products:
+            if product['id]==product_id:
+               print(f'-{product['name']}-${product['price']}')
+               total+=product['price]
+               print('---umumiy narx-----------')
+               print(total)
+    
+    
+    
+
 
